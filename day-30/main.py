384611668a189d2e36671527bae8919d5565d785
PC-Ngumoha <pcngumoha@gmail.com>
2025-08-04 11:49:44 +0100

    Progress on day 30

---
+++ day-30/main.py
@@ -0,0 +1,36 @@
+"""
+day 30: Exception Handling
+
+try:
+    code that may throw exception
+except:
+    code that will run if exception occurs
+else:
+    code that will run if there were no exceptions
+finally:
+    code that will run regardless.
+
+
+raise Exception
+
+"""
+
+# try:
+#     file = open("a_file.txt")
+#     # a_dict = {"key": "value"}
+#     # print(a_dict["none"])
+# # TIP: Always try to narrow your "except" clause to a specific exception type.
+# # NOTE: You can always have multiple "except" clauses for a single "try"
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_msg:
+#     print(f"The key {error_msg} does not exist.")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     # file.close()
+#     # print("File was closed")
+#     raise IndexError("I just want you to suffer 😂")
+
