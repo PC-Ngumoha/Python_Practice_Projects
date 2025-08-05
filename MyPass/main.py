5dde126b02296456a71cd03b0eaa0b8c68b3ef69
PC-Ngumoha <pcngumoha@gmail.com>
2025-08-05 05:28:52 +0100

    day-30 improvements

--- MyPass/main.py
+++ MyPass/main.py
@@ -1,6 +1,7 @@
 """
 main.py
 """
+import json
 from tkinter import *
 from tkinter import messagebox
 import random
@@ -35,23 +36,52 @@ def save_data():
     username = email_input.get()
     password = password_input.get()
 
+    new_data = {
+        website: {
+            "email": username,
+            "password": password,
+        }
+    }
+
     # Detect empty fields & notify user
     if len(website) == 0 or len(password) == 0:
         messagebox.showwarning("Oops", "All fields should be filled!")
     else:
-        # Prompt user to verify data entered
-        is_ok = messagebox.askokcancel(title=website,
-                                       message=f"Details entered:\nEmail: {username} "
-                                               f"\nPassword: {password} \nProceed to save?")
+        try:
+            with open('data.json', mode='r') as file:
+                # Reading old data
+                data = json.load(file)
+        except FileNotFoundError:
+            with open("data.json", mode='w') as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            # Updating old with new data
+            data.update(new_data)
 
-        if is_ok:
-            # Save data to file
-            with open('data.txt', mode='a') as file:
-                file.write(f"{website} | {username} | {password}\n")
+            with open('data.json', mode='w') as file:
+                # Saving updated data
+                json.dump(data, file, indent=4)
+        finally:
+            # Clear fields
+            website_input.delete(0, END)
+            password_input.delete(0, END)
 
-                # Clear fields
-                website_input.delete(0, END)
-                password_input.delete(0, END)
+# ---------------------------------------- FIND PASSWORD --------------------------------------------- #
+def find_password():
+    website_name = website_input.get()
+
+    try:
+        with open("data.json", mode='r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showwarning("Oops!", "No Data File Found")
+    else:
+        if website_name in data:
+            website = data[website_name]
+            messagebox.showinfo(website_name, f"Email: {website["email"]}"
+                                f"\nPassword:{website["password"]}")
+        else:
+            messagebox.showwarning("Oops", f"Entry '{website_name}' Not Found")
 
 # ---------------------------------------- UI SETUP -------------------------------------------------- #
 window = Tk()
@@ -70,8 +100,11 @@ website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
 website_input = Entry(width=35)
-website_input.grid(row=1, column=1, columnspan=2)
+website_input.grid(row=1, column=1)
 website_input.focus()
+
+search_btn = Button(text="Search", width=13, command=find_password)
+search_btn.grid(row=1, column=2)
 
 # Enter Email or Username
 email_label = Label(text="Email/Username:")
--- day-26/main.py
+++ day-26/main.py
@@ -19,6 +19,13 @@ data = pandas.read_csv('nato_phonetic_alphabet.csv')
 nato_alphabet = {letter:code for (_, (letter, code)) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter text: ").upper()
-result = [nato_alphabet[letter] for letter in user_input if letter != ' ']
-print(result)
+user_attempting = True
+while user_attempting:
+    try:
+        user_input = input("Enter text: ").upper()
+        result = [nato_alphabet[letter] for letter in user_input if letter != ' ']
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(result)
+        user_attempting = False
